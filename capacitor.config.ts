3d748c1c4e7a1a97070fdddc019f7265a17627c3
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-07-11 13:44:17 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Test Project One',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
