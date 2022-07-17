feaf94a3ae660c0e3b45eeedfe40ed859ebdb426
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-07-17 19:59:24 -0400

    adding card components

---
+++ src/app/components/pickup-card/pickup-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pickup-card',
+  templateUrl: './pickup-card.component.html',
+  styleUrls: ['./pickup-card.component.scss'],
+})
+export class PickupCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
